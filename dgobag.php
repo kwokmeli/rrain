c91f23ba77afcbc465019eb0bfa9c0d5c0b6f582
mkwok <mkwok@uw.edu>
2017-05-04 10:55:08 -0700

    started digital go bag template

---
+++ dgobag.php
@@ -0,0 +1,96 @@
+<?php /*Template Name: dgobag-page*/ ?>
+
+<!-- TODO: Incorrectly linked API? -->
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+<?php get_header(); ?>
+<div class="header-selection"><?php echo get_the_title(); ?></div>
+<div class="header-spacing"></div>
+<div class="pages">
+<?php echo get_post_field('post_content', $post->ID); ?>
+</div>
+
+<div class="sub-menu_titles">
+<?php if ( get_field('sub-menu_title_1') !== "" ) { ?>
+  <div class="dgobag-icon"><img src=<?php the_field('sub-menu_image_1');?>></div>
+  <div class="dgobag-info"><a href=<?php the_field('sub-menu_link_1'); ?>> <?php the_field('sub-menu_title_1'); ?> </a>
+  <br><?php the_field('sub-menu_excerpt_1'); ?>
+  <br><br><div class="dgobag-ios"><img src="https://testing.hsl.washington.edu/tisha/rrain-test-site/img/download_ios.png" style="border: #ffffff 3px double"></div><img src="https://testing.hsl.washington.edu/tisha/rrain-test-site/img/download_android2.png"></div>
+  <div class="sub-menu_icons"><i class="material-icons">&#xe315;</i></div>
+  <br><br>
+</div>
+  <div class="sub-menu_divider"></div>
+<?php } ?>
+<br>
+
+<div class="sub-menu_titles">
+<?php if ( get_field('sub-menu_title_2') !== "" ) { ?>
+  <a href=<?php the_field('sub-menu_link_2'); ?>> <?php the_field('sub-menu_title_2'); ?> </a>
+  <br><?php the_field('sub-menu_excerpt_2'); ?>
+  <div class="sub-menu_icons"><i class="material-icons">&#xe315;</i></div>
+  <br><br>
+</div>
+  <div class="sub-menu_divider"></div>
+<?php } ?>
+<br>
+
+<div class="sub-menu_titles">
+<?php if ( get_field('sub-menu_title_3') !== "" ) { ?>
+  <a href=<?php the_field('sub-menu_link_3'); ?>> <?php the_field('sub-menu_title_3'); ?> </a>
+  <br><?php the_field('sub-menu_excerpt_3'); ?>
+  <div class="sub-menu_icons"><i class="material-icons">&#xe315;</i></div>
+  <br><br>
+</div>
+  <div class="sub-menu_divider"></div>
+<?php } ?>
+<br>
+
+<div class="sub-menu_titles">
+<?php if ( get_field('sub-menu_title_4') !== "" ) { ?>
+  <a href=<?php the_field('sub-menu_link_4'); ?>> <?php the_field('sub-menu_title_4'); ?> </a>
+  <br><?php the_field('sub-menu_excerpt_4'); ?>
+  <div class="sub-menu_icons"><i class="material-icons">&#xe315;</i></div>
+  <br><br>
+</div>
+  <div class="sub-menu_divider"></div>
+<?php } ?>
+<br>
+
+<div class="sub-menu_titles">
+<?php if ( get_field('sub-menu_title_5') !== "" ) { ?>
+  <a href=<?php the_field('sub-menu_link_5'); ?>> <?php the_field('sub-menu_title_5'); ?> </a>
+  <br><?php the_field('sub-menu_excerpt_5'); ?>
+  <div class="sub-menu_icons"><i class="material-icons">&#xe315;</i></div>
+  <br><br>
+</div>
+  <div class="sub-menu_divider"></div>
+<?php } ?>
+<br>
+
+<div class="sub-menu_titles">
+<?php if ( get_field('sub-menu_title_6') !== "" ) { ?>
+  <a href=<?php the_field('sub-menu_link_6'); ?>> <?php the_field('sub-menu_title_6'); ?> </a>
+  <br><?php the_field('sub-menu_excerpt_6'); ?>
+  <div class="sub-menu_icons"><i class="material-icons">&#xe315;</i></div>
+  <br><br>
+</div>
+  <div class="sub-menu_divider"></div>
+<?php } ?>
+<br>
+
+<div class="sub-menu_titles">
+<?php if ( get_field('sub-menu_title_7') !== "" ) { ?>
+  <a href=<?php the_field('sub-menu_link_7'); ?>> <?php the_field('sub-menu_title_7'); ?> </a>
+  <br><?php the_field('sub-menu_excerpt_7'); ?>
+  <div class="sub-menu_icons"><i class="material-icons">&#xe315;</i></div>
+  <br><br>
+</div>
+  <div class="sub-menu_divider"></div>
+<?php } ?>
+<br>
+
+</div>
+<div class="header-spacing"></div>
+
+
+<?php wp_footer(); ?>
