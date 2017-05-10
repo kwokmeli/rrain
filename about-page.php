9c11b4e39fa46808ccdffeeeae256f844b37b25c
mkwok <mkwok@uw.edu>
2017-05-10 11:15:07 -0700

    fixed cemp padding, about-page padding

--- about-page.php
+++ about-page.php
@@ -3,7 +3,7 @@
 <?php get_header(); ?>
 <div class="header-selection"><?php echo get_the_title(); ?></div>
 <div class="header-spacing"></div>
-<div class="pages">
+<div class="about-page">
 <?php echo get_post_field('post_content', $post->ID); ?>
 </div>
 
