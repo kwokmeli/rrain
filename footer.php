5a1075905c97973c601767d45dad3ead7bc3cf25
mkwok <mkwok@uw.edu>
2017-05-16 11:01:38 -0700

    fixed footer text max-width

--- footer.php
+++ footer.php
@@ -6,8 +6,10 @@
   <img id="foot-img" src="https://testing.hsl.washington.edu/tisha/rrain-test-site/img/doh.jpg"style="height: 6%"/>
   <img id="foot-img" src="https://testing.hsl.washington.edu/tisha/rrain-test-site/img/nlm.png" style="height: 6%"/></a><br><br><br>
 
-  RRAIN Washington is a joint project between the UW Health Sciences Library and the WA Department of Health <br>
+<div class="footer-text">
+  RRAIN Washington is a joint project between the UW Health Sciences Library and the WA Department of Health
   with funding support from the NLM Contract No. HHS-N-276-2014-00658-P<br>
+</div>
 
   <div class="header-spacing"></div>
 </footer>
