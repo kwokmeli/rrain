769f2210d05f6a07c55d41527a72bcc5e076533d
Melissa Kwok <mkwok@D-172-25-122-192.dhcp4.washington.edu>
2017-06-13 14:33:20 -0700

    started dgobag template

--- test-page.php
+++ test-page.php
@@ -4,10 +4,12 @@
 
 <?php get_header(); ?>
 
-<?php if ( is_front_page() ) { ?>
-  <div class="blog-post-title"><?php echo get_post_field('post_content', $post->ID); ?></div>
-<?php } else { ?>
-  <div class="sub-title"><?php echo get_post_field('post_content', $post->ID); ?></div>
+<?php if ( is_front_page() ) { ?> <!-- CSS for index page -->
+        <div class="blog-post-title"><?php echo get_post_field('post_content', $post->ID); ?></div>
+<?php } else if ( is_page(102) ) { ?> <!-- CSS for Digital Go Bag page --> <!-- TODO: Switch out page ID -->
+          <div class="dgobag"><?php echo get_post_field('post_content', $post->ID); ?></div>
+<?php } else { ?> <!-- CSS for submenu page -->
+          <div class="sub-title"><?php echo get_post_field('post_content', $post->ID); ?></div>
 <?php } ?>
 
 <?php get_footer(); ?>
