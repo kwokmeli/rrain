49d5da3aa183e732c99458d3db30fc5b0f3e8d65
Melissa Kwok <mkwok@D-172-25-122-192.dhcp4.washington.edu>
2017-06-14 11:25:31 -0700

    finished dgobag page, cemp page

--- test-page.php
+++ test-page.php
@@ -7,9 +7,11 @@
 <?php if ( is_front_page() ) { ?> <!-- CSS for index page -->
         <div class="blog-post-title"><?php echo get_post_field('post_content', $post->ID); ?></div>
 <?php } else if ( is_page(102) ) { ?> <!-- CSS for Digital Go Bag page --> <!-- TODO: Switch out page ID -->
-          <div class="dgobag"><?php echo get_post_field('post_content', $post->ID); ?></div>
+        <div class="dgobag"><?php echo get_post_field('post_content', $post->ID); ?></div>
+<?php } else if ( is_page(133) ) { ?> <!-- CSS for CEMP page --> <!-- TODO: Switch out page ID -->
+        <div class="box"><div class="cemp"><?php echo get_post_field('post_content', $post->ID); ?></div></div>
 <?php } else { ?> <!-- CSS for submenu page -->
-          <div class="sub-title"><?php echo get_post_field('post_content', $post->ID); ?></div>
+        <div class="sub-title"><?php echo get_post_field('post_content', $post->ID); ?></div>
 <?php } ?>
 
 <?php get_footer(); ?>
