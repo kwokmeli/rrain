de4c615e183be79389235743fc331f9ba8346a16
Melissa Kwok <mkwok@D-172-25-122-192.dhcp4.washington.edu>
2017-06-14 12:56:43 -0700

    fixing window resize issues

--- index.php
+++ index.php
@@ -1,20 +1,10 @@
-<!-- TODO: Incorrectly linked API? -->
-<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-
 <?php get_header(); ?>
   <div class="row">
 
     <div class="col-sm-8 blog-main">
-      <div class="header-spacing"></div>
-      <?php
-			if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-        <?php get_template_part( 'content', get_post_format() );?>
-        <i class="material-icons">&#xe315;</i>
-        <div class="divider"></div>
-			<?php endwhile; endif; ?>
+
 
       </div><!-- /.blog-main -->
     </div><!-- /.row -->
-    <div class="header-spacing"></div>
 
 <?php get_footer(); ?>
--- test-page.php
+++ test-page.php
@@ -1,17 +1,32 @@
-<!-- TODO: Incorrectly linked API? -->
-<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 <?php /*Template Name: test-page*/ ?>
 
 <?php get_header(); ?>
 
 <?php if ( is_front_page() ) { ?> <!-- CSS for index page -->
-        <div class="blog-post-title"><?php echo get_post_field('post_content', $post->ID); ?></div>
+        <div class="blog-post-title">
+          <?php echo get_post_field('post_content', $post->ID); ?>
+        </div>
+        <?php get_footer(); ?>
 <?php } else if ( is_page(102) ) { ?> <!-- CSS for Digital Go Bag page --> <!-- TODO: Switch out page ID -->
-        <div class="dgobag"><?php echo get_post_field('post_content', $post->ID); ?></div>
-<?php } else if ( is_page(133) ) { ?> <!-- CSS for CEMP page --> <!-- TODO: Switch out page ID -->
-        <div class="box"><div class="cemp"><?php echo get_post_field('post_content', $post->ID); ?></div></div>
+        <div class="dgobag">
+          <?php echo get_post_field('post_content', $post->ID); ?>
+        </div>
+        <?php wp_footer(); ?>
+<?php } else if ( is_page(133) || is_page(158) ) { ?> <!-- CSS for CEMP, Events, Key Personnel, and Focus Group pages --> <!-- TODO: Switch out page ID -->
+        <div class="box">
+          <div class="cemp">
+            <?php echo get_post_field('post_content', $post->ID); ?>
+          </div>
+        </div>
+        <?php wp_footer(); ?>
+<?php } else if ( is_page(147) ) { ?> <!-- CSS for About page --> <!-- TODO: Switch out page ID -->
+        <div class="about">
+          <?php echo get_post_field('post_content', $post->ID); ?>
+        </div>
+        <?php get_footer(); ?>
 <?php } else { ?> <!-- CSS for submenu page -->
-        <div class="sub-title"><?php echo get_post_field('post_content', $post->ID); ?></div>
+        <div class="sub-title">
+          <?php echo get_post_field('post_content', $post->ID); ?>
+        </div>
+        <?php wp_footer(); ?>
 <?php } ?>
-
-<?php get_footer(); ?>
